7b30fe2fb3222c86daa35e926347d2184916455f
Shravani <shravani.b3@gmail.com>
2021-03-12 14:30:52 +1100

    styled index andn show listings, addded hero img to assets and home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+    def select_alert(input)
+        case input
+        when 'notice'
+            return 'info'
+        when 'success'
+            return 'success'
+        when 'error'
+            return 'danger'
+        when 'alert'
+            return 'warning'
+        else
+            return 'warning'
+        end
+    end
 end
